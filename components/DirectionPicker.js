1fc2e94d2720ab234f84c7649d8b6ad8dc04b0fd
Aaron Master <master.a@northeastern.edu>
2025-02-02 15:35:36 -0500

    added readme

--- components/DirectionPicker.js
+++ components/DirectionPicker.js
@@ -1,22 +1,30 @@
 import Button from './Button';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 const DirectionPicker = ({ isInbound, setIsInboundFunction, lineColor }) => {
   return (
     <View style={styles.buttonContainer}>
-      <Button
-        text={'↑'}
-        isActive={!isInbound}
-        buttonPressedFunction={() => setIsInboundFunction(false)}
-        lineColor={lineColor}
-      />
-      <Text style={styles.directonText}>Direction</Text>
-      <Button
-        text={'↓'}
-        isActive={isInbound}
-        buttonPressedFunction={() => setIsInboundFunction(true)}
-        lineColor={lineColor}
-      />
+      <Text style={styles.directionText}>Direction</Text>
+      {/* <Pressable onPress={() => setIsInboundFunction(false)}> */}
+      <Pressable onPress={() => console.log(false)}>
+        <Text style={styles.directionText}>↑</Text>
+      </Pressable>
+      {/* <Pressable onPress={() => setIsInboundFunction(true)}> */}
+      <Pressable onPress={() => console.log(true)}>
+        <Text style={styles.directionText}>↓</Text>
+      </Pressable>
+      {/* <Button */}
+      {/*   text={'↑'} */}
+      {/*   isActive={!isInbound} */}
+      {/*   buttonPressedFunction={() => setIsInboundFunction(false)} */}
+      {/*   lineColor={lineColor} */}
+      {/* /> */}
+      {/* <Button */}
+      {/*   text={'↓'} */}
+      {/*   isActive={isInbound} */}
+      {/*   buttonPressedFunction={() => setIsInboundFunction(true)} */}
+      {/*   lineColor={lineColor} */}
+      {/* /> */}
     </View>
   );
 };
@@ -28,7 +36,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 20,
   },
-  directonText: {
+  directionText: {
     fontSize: 20,
   },
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+MBTA Train Tracker App!
+
+To debug on Windows PC:
+nvm use node
+npx expo start --tunnel
