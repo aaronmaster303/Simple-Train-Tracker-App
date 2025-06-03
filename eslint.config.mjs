1f8ebb4065c675ebef37b9a036ca3b3fbe23ff06
Aaron Master <aaronmaster303.dev@gmail.com>
2025-06-02 21:50:31 -0400

    removed proptypes react error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,5 +8,9 @@ export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    rules: "react/prop-types": "offA"
+  },
   pluginReact.configs.flat.recommended,
 ];
