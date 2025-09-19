f50b73c73b7c46256ace02ee026fcbbdbea5415a
Aaron Master <aaronmaster303.dev@gmail.com>
2025-09-18 23:12:09 -0400

    show highlighted stop time element

--- components/Stop.js
+++ components/Stop.js
@@ -7,52 +7,73 @@ const statusStrings = {
 	INCOMING_AT: '(Incoming at)',
 };
 
-const Stop = ({ name, lineColor, status, stopTime }) => {
+const stopTimeElement = (name, stopTime, lineColor) => {
 	return (
-		<View>
-			{status ? (
-				<View style={[styles.stopContainer, { backgroundColor: lineColor.lighter }]}>
-					<Text
-						style={[
-							styles.stopText,
-							styles.stopTextActive,
-							{ color: lineColor.primary },
-						]}>
-						{name}
-					</Text>
-					<Text style={[styles.stopStatus, { color: lineColor.primary }]}>
-						{` ${statusStrings[status]} `}
-					</Text>
-					{stopTime && Object.keys(stopTime).length > 0 ? (
-						<Text style={[styles.stopStatus, { color: lineColor.primary }]}>
-							{` ${stopTime.minutes}:${stopTime.seconds} `}
-						</Text>
-					) : (
-						<></>
-					)}
-				</View>
-			) : (
-				<View style={styles.stopContainer}>
-					<Text style={styles.stopText}>{name}</Text>
-					{stopTime && Object.keys(stopTime).length > 0 ? (
-						<Text style={[styles.stopStatus, { color: lineColor.primary }]}>
-							{formatTime(stopTime.minutes, stopTime.seconds)}
-						</Text>
-					) : (
-						<></>
-					)}
-				</View>
-			)}
+		<View style={[styles.selectedStopContainer, { backgroundColor: lineColor.primary }]}>
+			<Text style={[styles.stopText, styles.stopTextActive, styles.stopTimeColor]}>
+				{name}
+			</Text>
+			<Text
+				style={[
+					styles.stopStatus,
+					styles.stopTextActive,
+					styles.stopTimeColor,
+					{ marginHorizontal: 12 },
+				]}>
+				{formatTime(stopTime.minutes, stopTime.seconds)}
+			</Text>
+		</View>
+	);
+};
+
+const activeStopElement = (name, status, lineColor) => {
+	return (
+		<View style={[styles.stopContainer, { backgroundColor: lineColor.lighter }]}>
+			<Text style={[styles.stopText, styles.stopTextActive, { color: lineColor.primary }]}>
+				{name}
+			</Text>
+			<Text style={[styles.stopStatus, { color: lineColor.primary }]}>
+				{` ${statusStrings[status]} `}
+			</Text>
+		</View>
+	);
+};
+
+const defaultStopElement = (name) => {
+	return (
+		<View style={[styles.stopContainer]}>
+			<Text style={[styles.stopText]}>{name}</Text>
 		</View>
 	);
 };
 
 const formatTime = (minutes, seconds) => {
-	return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+	return `➔ ${minutes <= 1 ? '1' : minutes}min`;
+};
+
+const Stop = ({ name, lineColor, status, stopTime }) => {
+	return (
+		<View>
+			{stopTime && Object.keys(stopTime).length > 0
+				? stopTimeElement(name, stopTime, lineColor)
+				: status
+					? activeStopElement(name, status, lineColor)
+					: defaultStopElement(name)}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
 	stopContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingVertical: 8,
+		marginVertical: 1,
+		borderRadius: 7,
+		flexWrap: 'wrap',
+	},
+	selectedStopContainer: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		alignItems: 'center',
@@ -75,6 +96,9 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		color: '#28a745',
 		marginHorizontal: 6,
+	},
+	stopTimeColor: {
+		color: 'white',
 	},
 });
 
